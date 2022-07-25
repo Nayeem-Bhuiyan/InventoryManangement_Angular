d860f78f38366e4421e65a9d03dcc43f545f9989
Nayeem-Bhuiyan <nayeembhuiyan40@gmail.com>
2022-07-25 10:05:26 +0600

    ghjghjghjghj

--- NayeemApp.Domain/Entity/User.cs
+++ NayeemApp.Domain/Entity/User.cs
@@ -8,13 +8,13 @@ namespace NayeemApp.Domain.Entity
 {
     public class User
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string CurrentPassword { get; set; }
-        public string CreatedBy { get; set; }
-        public string Role { get; set; }
-        public string UserId { get; set; }
+        public string FirstName { get; set;}
+        public string LastName { get; set;}
+        public string Email { get; set;}
+        public string Password { get; set;}
+        public string CurrentPassword { get; set;}
+        public string CreatedBy { get; set;}
+        public string Role { get; set;}
+        public string UserId { get; set;}
     }
 }
