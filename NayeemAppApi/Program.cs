a7efc9b775f9868dd96a47dc127ee04b7cbcfacf
Nayeem-Bhuiyan <nayeembhuiyan40@gmail.com>
2022-07-25 11:30:17 +0600

    nm,nbm,nm,

--- NayeemAppApi/Program.cs
+++ NayeemAppApi/Program.cs
@@ -69,7 +69,7 @@ builder.Services.AddAuthentication(x =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddMvc();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
